f8dfcb71b77c12b5da449aa29c63cb409e3a0b1d
Chris <christianotu.d@gmail.com>
2022-03-28 22:36:41 +0100

    Updated with app providers

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,14 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app";
+import { AppProvider } from "@/providers/app";
+
+import "@/styles/style.scss";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <AppProvider>
+      <Component {...pageProps} />
+    </AppProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
